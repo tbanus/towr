f919681872713fc89571431d8fa3eb3030ae7348
tbanus <omertarikbanus@gmail.com>
2024-12-27 19:35:21 +0300

    minor stuff docker

--- docker_launch.sh
+++ docker_launch.sh
@@ -3,7 +3,8 @@ docker run -it --rm \
     -e XAUTHORITY=/tmp/.docker.xauth \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $XAUTHORITY:/tmp/.docker.xauth \
+    -v /home/$USER/towr_ws/towr:/app \
     --net=host \
-    towr:ros
+    towr:1205
 
-roslaunch towr_ros towr_ros.launch
+# roslaunch towr_ros towr_ros.launch
